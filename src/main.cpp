c9128c248d180aefbcd1f1688bbf33511c880699
minguen <mindal99546@gmail.com>
2021-12-30 18:47:50 +0900

    replaced deprecated codes

--- src/main.cpp
+++ src/main.cpp
@@ -2,55 +2,52 @@
 #include <Arduino.h>
 #include <IRremote.h>
 
-int recvPin=2;
-IRrecv irrecv(recvPin);
+int recvPin = 2;
+IRrecv irrecv;
+IRData* irdata;
 
 void setup() {
+  irrecv.begin(recvPin, true);
   Serial.begin(9600);
-  irrecv.enableIRIn();
 }
 
-void  ircode(decode_results* results){
-  // if(results->decode_type==SAMSUNG){
-  //   Serial.print(results->address, HEX);
-  //   Serial.print(":");
-  // }
-  Serial.print(results->value, HEX);
-}
-
-void encoding(decode_results* results){
-  switch (results->decode_type)
-  {
-  case UNKNOWN: Serial.print("UNKNOWN"); break;
+void manufacturer(IRData* irdata){
+  switch (irdata->protocol){
   case SAMSUNG: Serial.print("SAMSUNG"); break;
-  case LG: Serial.print("LG"); break;
+  case LG: Serial.print("LG     "); break;
   default: Serial.print("DEFAULT"); break;
   }
 }
 
-void dumpInfo(decode_results* results){
-  if(results->overflow){
-    Serial.println("IR code too long. Edit IRremoteInt.h and increase RAWLEN");
-    return ;
-  }
-  //Show encoding standard
-  Serial.print("Encoding : ");
-  encoding(results);
-  Serial.print(" ");
-  //Show code&length
-  Serial.print("Code : ");
-  ircode(results);
-  Serial.print(" (");
-  Serial.print(results->bits, DEC);
-  Serial.println(" bits)");
+void command(IRData* irdata){
+  manufacturer(irdata);
+  Serial.print('\t');
+  Serial.print("\t address: ");
+  Serial.print(irdata->address, HEX);
+  Serial.print("\t command: ");
+  Serial.print(irdata->command, HEX);
+  Serial.print("\t extra: ");
+  Serial.print(irdata->extra, HEX);
+  Serial.print("\t numberOfBits: ");
+  Serial.print(irdata->numberOfBits, HEX);
+  Serial.print("\t flags: ");
+  Serial.print(irdata->flags, HEX);
+  Serial.print("\t decodedRawData: ");
+  Serial.print(irdata->decodedRawData, HEX);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  decode_results results;
-  if(irrecv.decode(&results)){
-    dumpInfo(&results);
-    Serial.println("");
-    irrecv.resume();
+  if(irrecv.decode()){
+   irdata=irrecv.read();
+   if(irdata->flags==0x40){
+     Serial.print("OverFlowed\n");
+     return ;
+   }
+  //  manufacturer(irdata);
+  //  Serial.print('\t');
+   command(irdata);
+   Serial.print('\n');
+   irrecv.resume();
   }
+  irdata=nullptr;
 }
