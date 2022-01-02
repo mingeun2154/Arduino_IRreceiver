59c32ffd5a8a1d4a12a0c8303888d6627f4c8166
minguen <mindal99546@gmail.com>
2022-01-03 01:34:41 +0900

    receive signal and turn on led

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,35 @@
-//receiver
+//Receiver
 #include <Arduino.h>
-#include <IRremote.h>
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
 
-int recvPin = 2;
-IRrecv irrecv;
-IRData* irdata;
+RF24 radio(9, 10);
+const byte address[6]="00001"; //Address value must be same in sender and receiver
+int led_pin=7;
 
 void setup() {
-  irrecv.begin(recvPin, true);
   Serial.begin(9600);
-}
-
-void manufacturer(IRData* irdata){
-  switch (irdata->protocol){
-  case SAMSUNG: Serial.print("SAMSUNG"); break;
-  case LG: Serial.print("LG     "); break;
-  default: Serial.print("DEFAULT"); break;
-  }
-}
-
-void command(IRData* irdata){
-  manufacturer(irdata);
-  Serial.print('\t');
-  Serial.print("\t address: ");
-  Serial.print(irdata->address, HEX);
-  Serial.print("\t command: ");
-  Serial.print(irdata->command, HEX);
-  Serial.print("\t extra: ");
-  Serial.print(irdata->extra, HEX);
-  Serial.print("\t numberOfBits: ");
-  Serial.print(irdata->numberOfBits, HEX);
-  Serial.print("\t flags: ");
-  Serial.print(irdata->flags, HEX);
-  Serial.print("\t decodedRawData: ");
-  Serial.print(irdata->decodedRawData, HEX);
+  radio.begin();
+  //RF
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening(); //Set this module as receiver.
+  //led
+  pinMode(led_pin, OUTPUT);
 }
 
 void loop() {
-  if(irrecv.decode()){
-   irdata=irrecv.read();
-   if(irdata->flags==0x40){
-     Serial.print("OverFlowed\n");
-     return ;
-   }
-  //  manufacturer(irdata);
-  //  Serial.print('\t');
-   command(irdata);
-   Serial.print('\n');
-   irrecv.resume();
+  if(radio.available()){
+    digitalWrite(led_pin, HIGH);
+    char text[32]="";
+    radio.read(text, sizeof(text));
+    Serial.println(text);
+    delay(1000);
   }
-  irdata=nullptr;
+  else{
+    digitalWrite(led_pin, LOW);
+    Serial.println("nothing received");
+    delay(1000);
+  }
 }
