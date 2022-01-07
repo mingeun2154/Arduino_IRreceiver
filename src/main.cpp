7eddfc5f66af6cc9a8a64cffc6e7421f823ee207
minguen <mindal99546@gmail.com>
2022-01-07 23:01:37 +0900

    feat:control servo motors.

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,61 @@
 #include <SPI.h>
 #include <RF24.h>
 #include <nRF24L01.h>
+#include <Servo.h>
+#include <string.h>
 
+#define ON "on"//right
+#define OFF "off"//left
+
+//RF module.
 RF24 radio(9, 10);
 const byte address[6]="00001"; //Address value must be same in sender and receiver
-int led_pin=7;
+//servo motors.
+Servo leftServo;
+Servo rightServo;
+int leftServo_pin=4;
+int rightServo_pin=5;
+bool isOn_first=0;
+bool isOn_second=0;
 
 void setup() {
   Serial.begin(9600);
   radio.begin();
-  //RF
+  //RF module.
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening(); //Set this module as receiver.
-  //led
-  pinMode(led_pin, OUTPUT);
+  //servo motors.
+  pinMode(leftServo_pin, OUTPUT);
+  pinMode(rightServo_pin, OUTPUT);
+  leftServo.attach(leftServo_pin);
+  rightServo.attach(rightServo_pin);
 }
 
 void loop() {
+  char signal[16]="";
+  //Initialize servo motors angle to 0.
+  if(leftServo.read()!=0){
+    leftServo.write(0); 
+    delay(100);
+  }
+  if(rightServo.read()!=0){
+    rightServo.write(0); 
+    delay(100);
+  }
+  //Receive signal.
   if(radio.available()){
-    digitalWrite(led_pin, HIGH);
-    char text[32]="";
-    radio.read(text, sizeof(text));
-    Serial.println(text);
-    delay(1000);
+    char signal[16]="";
+    radio.read(&signal, sizeof(signal));
+    if(!strcmp(signal, OFF)){  //strcmp() return 0 if they are same strings.
+      leftServo.write(30); 
+      delay(500);
+    }
+    else if(!strcmp(signal, ON)){
+      rightServo.write(35); 
+      delay(500);
+    }
+    else;
   }
-  else{
-    digitalWrite(led_pin, LOW);
-    Serial.println("nothing received");
-    delay(1000);
-  }
+
 }
